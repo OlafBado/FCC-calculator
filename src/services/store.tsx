7a9d1059f5138306055605b9409c6668c64ab66c
OlafWindrunner <niebiel1@gmail.com>
2022-11-07 22:35:50 +0100

    fix

--- src/services/store.tsx
+++ src/services/store.tsx
@@ -1,42 +1,5 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-// const parser = (str: string) => {
-//     switch(str) {
-//         case '1':
-//             return 1
-//             break
-//         case '2':
-//             return 2
-//             break
-//         case '3':
-//             return 3
-//             break
-//         case '4':
-//             return 4
-//             break
-//         case '5':
-//             return 5
-//             break
-//         case '6':
-//             return 6
-//             break
-//         case '7':
-//             return 7
-//             break
-//         case '8':
-//             return 8
-//             break
-//         case '9':
-//             return 9
-//             break
-//         case '/':
-//             return /
-//             break
-
-        
-//     }
-// }
-
 const displaySlice = createSlice({
     name: 'count',
     initialState: {
